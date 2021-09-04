7ec0a8526b19047f1e38210e5f1a89ba7afa028c
SonTrungTo <son.trung.to@gmail.com>
2021-09-04 14:20:19 +0300

    ch2, in progress...

--- chapter2/README.md
+++ chapter2/README.md
@@ -5,3 +5,6 @@
 ## Terraform
 - Written in HCL (HashiCorps Configuration Language)
 - Start with `main.tf`
+- Note `implicit dependency` between resources (references), use `terraform graph`
+- Cidr Range Calculator: `ipaddressguide.com/cidr`
+- Graph dependency graphics for DOT: `GraphvizOnline`
--- chapter2/main.tf
+++ chapter2/main.tf
@@ -5,8 +5,32 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-087c17d1fe0178315"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "${aws_security_group.instance.id}" ]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "Hello world" > index.html
+  nohup python -m SimpleHTTPServer 8080 &
+  EOF
 
   tags = {
     Name = "terraform-example"
   }
 }
+
+## AWS needs to be allowed for incoming/outgoing traffic
+## from EC2
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress = [ {
+    cidr_blocks = [ "0.0.0.0/0" ]
+    description = "Opening incoming/outgoing traffic to EC2"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+  } ]
+}
+
+## We must use the security group on the VPC security group ids
+## of the AWS Instance
