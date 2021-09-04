e10cae929d79b5e7081e1fe06c62a08278c6b58e
SonTrungTo <son.trung.to@gmail.com>
2021-09-04 16:46:40 +0300

    ch2 in progress...

--- chapter2/README.md
+++ chapter2/README.md
@@ -8,3 +8,9 @@
 - Note `implicit dependency` between resources (references), use `terraform graph`
 - Cidr Range Calculator: `ipaddressguide.com/cidr`
 - Graph dependency graphics for DOT: `GraphvizOnline`
+
+## Deploying to a cluster of servers
+- Using Auto Scaling Group (ASG) by AWS
+- First is to make configurations `aws_launch_configuration`
+- `lifecycle` config is needed for ASG.
+    - `create_before_destroy(bool)`: create a replacement before destroying the original resource.(must be on dependent resources as well)
--- chapter2/main.tf
+++ chapter2/main.tf
@@ -2,6 +2,41 @@ provider "aws" {
     region = "us-east-1"
 }
 
+data "aws_availability_zones" "all" {}
+data "aws_iam_user" "current" {
+  user_name = "terraform-tutorial"
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-087c17d1fe0178315"
+  instance_type = "t2.micro"
+  security_groups = [ "${aws_security_group.instance.id}" ]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  echo "Hello world" > index.html
+  nohup python -m SimpleHTTPServer "${var.server_port}" &
+  EOF
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones   = "${data.aws_availability_zones.all.names}"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-087c17d1fe0178315"
   instance_type = "t2.micro"
@@ -10,7 +45,7 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
   #!/bin/bash
   echo "Hello world" > index.html
-  nohup python -m SimpleHTTPServer 8080 &
+  nohup python -m SimpleHTTPServer "${var.server_port}" &
   EOF
 
   tags = {
@@ -23,14 +58,30 @@ resource "aws_instance" "example" {
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
-  ingress = [ {
-    cidr_blocks = [ "0.0.0.0/0" ]
-    description = "Opening incoming/outgoing traffic to EC2"
-    from_port   = 8080
-    to_port     = 8080
+  ingress {
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
-  } ]
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 ## We must use the security group on the VPC security group ids
 ## of the AWS Instance
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default     = 8080
+}
+
+output "public_ip" {
+  value = "${aws_instance.example.public_ip}"
+}
+
+output "current_user" {
+  value = "${data.aws_iam_user.current}"
+}
